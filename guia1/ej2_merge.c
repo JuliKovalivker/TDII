6d7661985865765dc0063b8bf92d6144c38d69e8
JuliKovalivker <101721164+JuliKovalivker@users.noreply.github.com>
2024-10-15 12:02:18 -0300

    guia 1, ej2

--- guia1/ej2_merge.c
+++ guia1/ej2_merge.c
@@ -1,32 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 int* merge(int* A, int sizeA, int* B, int sizeB) {
 
-    // COMPLETAR
+	int lon = sizeA + sizeB;
+    int* c = (int*) malloc( sizeof(int) * lon); 
+	int i = 0;
+	int j = 0;
+	while(A[i]!=0){
+		c[i] = A[i];
+		i++;
+	}
+	while(B[j]!=0){
+		c[i+j] = B[j];
+		j++;
+	}
+	c[i+j] = 0;
+	
+	int menor = 0;
+	int temp = 0;
+	for (int i = 0; i<lon; i++){
+		menor = i;
+		for(int j = i; j<lon; j++){
+			if (c[menor] > c[j]){
+				menor = j;
+			}
+		}
+		temp = c[i];
+		c[i] = c[menor];
+		c[menor] = temp;
+	}
     
-    return 0;
+    return c;
 }
 
 int main() {
-    /*
-    // -- Descomentar para probar --
-    // Lo siguiente es un ejemplo y DEBE ser modificado.
 
     int a[5] = {4,5,6,7,8};
-    int b[4] = {1,2,3,9};
+    int b[5] = {1,2,-2,9,6};
 
-    int* result = merge(a, 5, b, 4);
+    int* result = merge(a, 5, b, 5);
 
     printf("Result:");
-    for(int i=0; i<9; i++) {
+    for(int i=0; i<10; i++) {
         printf(" %i", result[i]);
     }
     printf("\n");
 
     free(result);
-
-    // */
 
     return 0;
 }
